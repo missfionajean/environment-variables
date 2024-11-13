69b36ce14f257fec4be1a091fc3ace62b69902f7
missfionajean <missfionajean@gmail.com>
2024-11-13 14:59:24 -0500

    added .env and gitignore to pass over it

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-require('dotenv').config();
+// goes at very top of JS file
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('The server is running');
+app.get("/", (req, res) => {
+	res.send("The server is running");
 });
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+	console.log("Listening on port 3000");
+	// grabs info from .env file
+	console.log(`Your password is ${process.env.SECRET_PASSWORD}`);
 });
